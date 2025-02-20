00b99bd298331a4249ea9fbd499e54ca7ad6ea9e
myr-onl <140292626+myr-onl@users.noreply.github.com>
2025-02-20 00:05:12 -0500

    Self-hosting prep
    - Modded layout to account for using a subdomain
    - Removed inject statement from layout script that prevented dev build (unsure of the consequences of this)
    - Removed Google ads
    - Removed Vercel analytics (layout code only, probs still installed with npm)
    - Removed dead security link
    - Fixed dead contact link
    - Fixed poor color contrast in select drop downs
    - Added sample .env template using the variables from my modded layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,13 @@
   import source from "$lib/assets/source.svg";
   import email from "$lib/assets/email.svg";
   import heart from "$lib/assets/heart.svg";
-  import { inject } from "@vercel/analytics";
   import { dev } from "$app/environment";
 
-  inject({ mode: dev ? "development" : "production" });
+  let baseUrl = import.meta.env.VITE_BASE_URL || "https://vanish.so";
+  let subdomain = import.meta.env.VITE_SITE_SUBDOMAIN;
+  let domain = import.meta.env.VITE_SITE_DOMAIN || "vanish";
+  let tld = import.meta.env.VITE_SITE_TLD || "so"
+  let mailto = import.meta.env.VITE_ADMIN_EMAIL;
 </script>
 
 <svelte:head>
@@ -17,24 +20,24 @@
     content="open source end-to-end encrypted vanishing notes"
   />
 
-  <meta property="og:url" content="https://www.vanish.so/" />
+  <meta property="og:url" content="{baseUrl}" />
   <meta property="og:type" content="website" />
   <meta property="og:title" content="vanish.so - secure vanishing notes" />
   <meta
     property="og:description"
     content="open source end-to-end encrypted vanishing notes"
   />
-  <meta property="og:image" content="https://www.vanish.so/og.png" />
+  <meta property="og:image" content="{baseUrl}/og.png" />
 
   <meta name="twitter:card" content="summary_large_image" />
-  <meta property="twitter:domain" content="vanish.so" />
-  <meta property="twitter:url" content="https://www.vanish.so/" />
+  {#if subdomain} <meta property="twitter:domain" content="{subdomain}.{domain}.{tld}" /> {:else} <meta property="twitter:domain" content="{domain}.{tld}" /> {/if}
+  <meta property="twitter:url" content="{baseUrl}" />
   <meta name="twitter:title" content="vanish.so - secure vanishing notes" />
   <meta
     name="twitter:description"
     content="open source end-to-end encrypted vanishing notes"
   />
-  <meta name="twitter:image" content="https://www.vanish.so/og.png" />
+  <meta name="twitter:image" content="{baseUrl}/og.png" />
 </svelte:head>
 <main
   class="w-screen h-screen overflow-hidden flex flex-col items-center relative"
@@ -48,18 +51,18 @@
   <div class="flex items-center justify-between w-full max-w-5xl my-6 px-10">
     <a href="/" data-sveltekit-reload>
       <h1 class="font-semibold text-white font-clash text-2xl">
-        vanish<span class="text-[#BCA4F5]/30">.</span><span
-          class="text-[#8F69FA]">so</span
-        >
+      {#if subdomain} {subdomain}
+        <span class="text-[#BCA4F5]/30">.</span>
+        <span class="text-[#8F69FA]">{domain}</span>
+        <span class="text-[#BCA4F5]/30">.</span>
+        <span class="text-[#8F69FA]">{tld}</span>
+      {:else} {domain}
+        <span class="text-[#BCA4F5]/30">.</span>
+        <span class="text-[#8F69FA]">{tld}</span>
+      {/if}
       </h1>
     </a>
     <div class="flex items-center gap-6 text-primary font-medium text-sm">
-      <a href="/" class="flex gap-2 items-center">
-        <img class="h-4 w-4" alt="security icon" src={security} />
-        <p class="hover:text-white transition-colors hidden sm:block">
-          security
-        </p>
-      </a>
       <a
         href="https://github.com/bvvst/vanishso"
         class="flex gap-2 items-center"
@@ -69,30 +72,13 @@
           source code
         </p>
       </a>
-      <a href="/" class="flex gap-2 items-center">
+      {#if mailto} <a href="mailto:{mailto}" class="flex gap-2 items-center">
         <img class="h-4 w-4" alt="email icon" src={email} />
         <p class="hover:text-white transition-colors hidden sm:block">
           contact
         </p>
-      </a>
+      </a> {:else} {/if}
     </div>
   </div>
   <slot />
-  <script
-    async
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7783535633029345"
-    crossorigin="anonymous"
-  ></script>
-  <!-- r -->
-  <ins
-    class="adsbygoogle"
-    style="display:block"
-    data-ad-client="ca-pub-7783535633029345"
-    data-ad-slot="6198772991"
-    data-ad-format="auto"
-    data-full-width-responsive="true"
-  />
-  <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-  </script>
 </main>
